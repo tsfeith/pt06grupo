10727964a8ef2ed93e1a43af34a11a27d462b2f3
nunorodrigues18 <36621629+nunorodrigues18@users.noreply.github.com>
2018-04-11 09:17:39 +0100

    Add files via upload

---
+++ numeromenor5.js
@@ -0,0 +1,11 @@
+
+var naoprimo = false;
+while (naoprimo == false) {
+
+   var numero = prompt("Escolha um número inferior a 5");
+   if (numero >5) {
+	   alert("O número tem de ser menor que 5");
+   } else {
+	   naoprimo = true;
+   }
+}
